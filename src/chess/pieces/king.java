fd1dbfadf1459a8780e52e6190a07ed1b452a485
Isaque Freitas <isaquefmateus@gmail.com>
2023-04-07 01:36:28 +0100

    Switching player each turn

--- src/chess/pieces/king.java
+++ src/chess/pieces/king.java
@@ -17,7 +17,7 @@ public class king extends ChessPiece {
 	}
 	private boolean canMove (Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
-		return p != null && p.getColor() != getColor();
+		return p == null || p.getColor() != getColor();
 	}
 
 	@Override
