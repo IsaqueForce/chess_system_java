aec228d17d2325971321495e1ecbb5978b2ef039
Isaque Freitas <isaquefmateus@gmail.com>
2023-04-18 03:00:44 +0100

    Checkmate Logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,12 +56,19 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
+		
+		if(!chessMatch.getCheckMate()) {
+		
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 		
 		if(chessMatch.getCheck()) {
 			System.out.println("Check!");
 		}
-		
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("WINNER: " + chessMatch.getCurrentPlayer());
+		}
 	}
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
